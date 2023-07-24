f8ed3b42bc1add484397dc74edca5c1c83a71a8d
Josh Thor <wunjosh@gmail.com>
2023-07-24 21:13:55 +0800

    WIP cpp03 ex00

---
+++ cpp03/ex00/srcs/ClapTrap.cpp
@@ -0,0 +1,93 @@
+#include "../includes/ClapTrap.hpp"
+
+/* Constructors and Destructor */
+ClapTrap::ClapTrap(void)
+	: _name(0), _hp(10), _ep(10), _attack_dmg(0)
+{
+	std::cout << "[Default constructor] ClapTrap unit with no name has spawned!" << '\n';
+}
+
+ClapTrap::ClapTrap(std::string given_name)
+	: _name(given_name), _hp(10), _ep(10), _attack_dmg(0)
+{
+	std::cout << "[Named Constructor] ClapTrap unit named " << given_name << " has spawned!" << '\n';
+}
+
+ClapTrap::ClapTrap(const ClapTrap &src) {
+	*this = src;
+	std::cout << "[Copy Constructor] ClapTrap unit named " << this->_name << " has been cloned!" << '\n';
+}
+
+ClapTrap::~ClapTrap(void) {
+	std::cout << "ClapTrap unit named " << this->_name << " has despawned!" << '\n';
+}
+
+/* Operator Overload */
+// ClapTrap	&ClapTrap::operator = (const ClapTrap &a) {
+// 	std::cout << "Copy assignment operator called" << '\n';
+// 	this->_name = 
+// }
+
+
+/* Member Functions */
+void	ClapTrap::attack(const std::string &target) {
+	if (this->_hp == 0) {
+		std::cout << "ClapTrap " << this->_name << " is already dead!" << '\n';
+		return ;
+	}
+	if (this->_ep == 0) {
+		std::cout << "ClapTrap " << this->_name << " has no more energy left!" << '\n';
+	}
+	else {
+		this->_ep -= 1;
+		std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_attack_dmg << " points of damage!" << '\n';
+	}
+}
+
+void	ClapTrap::takeDamage(unsigned int amount) {
+	if (this->_hp == 0) {
+		std::cout << "Oh, have mercy! ClapTrap " << this->_name << " is already dead!" << '\n';
+		return ;
+	}
+	std::cout << "ClapTrap " << this->_name << " has taken " << amount << " damage!" << '\n';
+	if (amount >= this->_hp) {
+		std::cout << "ClapTrap " << this->_name << " has died!" << '\n';
+		this->_hp = 0;
+	}
+	else {
+		this->_hp -= amount;
+		std::cout << "ClapTrap " << this->_name << " has " << this->_hp << " hit points left.";
+	}
+}
+
+void	ClapTrap::beRepaired(unsigned int amount) {
+	if (this->_hp == 0) {
+		std::cout << "ClapTrap " << this->_name << " is already dead!" << '\n';
+		return ;
+	}
+	if (this->_ep == 0) {
+		std::cout << "ClapTrap " << this->_name << " has no more energy left!" << '\n';
+	}
+	else {
+		std::cout << "ClapTrap " << this->_name << " has been repaired by " << amount << " hit points!" << '\n';
+		this->_hp += amount;
+		std::cout << "ClapTrap " << this->_name << " now has " << this->_hp << "hit points"; 
+	}
+}
+
+/* Getter Methods */
+std::string	ClapTrap::getName(void) const {
+	return (this->_name);
+}
+
+int	ClapTrap::getHitPoints(void) const {
+	return (this->_hp);
+}
+
+int	ClapTrap::getEnergyPoints(void) const {
+	return (this->_ep);
+}
+
+int	ClapTrap::getAttackDmg(void) const {
+	return (this->_attack_dmg);
+}
