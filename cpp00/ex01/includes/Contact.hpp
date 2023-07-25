66c3e0a7a8c6c1d84b709a6daed898e73ffb50e5
Josh Thor <wunjosh@gmail.com>
2023-07-25 21:53:35 +0800

    Redo cpp00 ex01

--- cpp00/ex01/includes/Contact.hpp
+++ cpp00/ex01/includes/Contact.hpp
@@ -1,22 +1,6 @@
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
 
-#include <iostream>
-#include "main.hpp"
 
-class Contact {
-public:
-	STRING	first_name;
-	STRING	last_name;
-	STRING	nickname;
-	STRING	phone_number;
-	STRING	darkest_secret;
-
-	Contact();
-	~Contact(void);
-
-	void	setContact(STRING f_n, STRING l_n, STRING n_n, STRING p_n, STRING d_s);
-	void	displayContact(int index) const;
-};
 
 #endif
--- cpp00/ex01/includes/PhoneBook.hpp
+++ cpp00/ex01/includes/PhoneBook.hpp
@@ -1,20 +1,6 @@
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
-#include <iostream>
-#include "main.hpp"
 
-class PhoneBook {
-public:
-	int		total_contacts;
-	Contact	contact_list[8];
-
-	PhoneBook(void);
-	~PhoneBook(void);
-
-	void	displayContacts(void) const;
-	void	displayEntry(int index) const;
-	void	addContact(void);
-};
 
 #endif
